e4b59952ada37a3845297d52db8f8e0395d788fa
“Thor” <hmkthor@gmail.com>
2022-03-23 22:45:35 +0900

    주석

--- carriage_return_test.sh
+++ carriage_return_test.sh
@@ -1,7 +1,7 @@
 #! /bin/bash^M
 
 # This file contains carriage returns.^M
-# 캐리지 리턴은 Dos에서 작성된 파일을 리눅스(유닉스)에서 볼때 생성 될 수 있고, 해당 내용 확인하려면 cat -v 명령어 사용한다.
+# 캐리지 리턴은 Dos에서 작성된 파일을 리눅스(유닉스)에서 볼때 생성 될 수 있고(or 윈도우파일을 리눅스파일에 내용 복사 시), 해당 내용 확인하려면 cat -v 명령어 사용한다.
 echo "Hello world."
 
 # file 명령어도 가능
